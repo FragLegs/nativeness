23fc2e131b9acf0fe618d34fa17ca96066db6d1c
FragLegs <miel.shayne@gmail.com>
2017-03-05 21:52:14 -0500

    fix majority bug

--- nativeness/models/majority.py
+++ nativeness/models/majority.py
@@ -1,8 +1,6 @@
 # -*- coding: utf-8 -*-
 import logging
 
-import numpy as np
-
 from nativeness.models.base import NativenessModel
 
 log = logging.getLogger(name=__name__)
@@ -12,5 +10,5 @@ class Majority(NativenessModel):
     def train(self, train_data, dev_data):
         self.majority = float(train_data.non_native.sum() >= len(train_data))
 
-    def predict(self, essays):
-        return np.array([self.majority for e in essays])
+    def predict(self, windows):
+        return self.majority
